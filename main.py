37e323ba658a1980322982d08196a702c580b8e8
Paolo Niccolò Giubelli <paoloniccolo.giubelli@gmail.com>
2021-01-06 22:31:57 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import facebook
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_lastday_impressions(page_id):
+    graph = facebook.GraphAPI(access_token=os.getenv("ACCESS_TOKEN"))
+    page_impressions = graph.get_connections(id=page_id, connection_name='insights',metric='page_impressions')
+    return page_impressions['data'][0]['values'][1]['value']
+
+def get_lastday_engagement(page_id):
+    graph = facebook.GraphAPI(access_token=os.getenv("ACCESS_TOKEN"))
+    page_impressions = graph.get_connections(id=page_id, connection_name='insights',metric='page_post_engagements*')
+    return page_impressions['data'][0]
+    
+val = get_lastday_engagement(os.getenv("PAGE_ID"))
+print(val)
