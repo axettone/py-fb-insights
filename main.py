0abda231e0d44af6adb9bc88fbae999f2426c5d1
Paolo Niccolò Giubelli <paoloniccolo.giubelli@gmail.com>
2021-01-07 08:43:46 +0100

    Added requirements.txt and a docker-compose file for database

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def get_lastday_impressions(page_id):
 
 def get_lastday_engagement(page_id):
     graph = facebook.GraphAPI(access_token=os.getenv("ACCESS_TOKEN"))
-    page_impressions = graph.get_connections(id=page_id, connection_name='insights',metric='page_post_engagements*')
-    return page_impressions['data'][0]
+    page_impressions = graph.get_connections(id=page_id, connection_name='insights',metric='page_impressions_viral',period='week')
+    return page_impressions
     
 val = get_lastday_engagement(os.getenv("PAGE_ID"))
 print(val)
